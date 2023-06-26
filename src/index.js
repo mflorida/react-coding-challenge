d927a9edb24d7521c03a554ed06c8bb2b702c1f0
Mark M. Florida <4173034+mflorida@users.noreply.github.com>
2023-06-25 20:12:42 -0500

    Cleanup index.js

--- src/index.js
+++ src/index.js
@@ -1,17 +1,8 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import MessageList from './components/message-list'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import MessageList from './components/message-list';
 
-const NewApp = require('./components/message-list').default
-
-function renderApp(App) {
-  ReactDOM.render(<App />, document.getElementById('root'))
-}
-
-renderApp(MessageList)
-
-if (module.hot) {
-  module.hot.accept('./components/message-list', () => {
-    renderApp(NewApp)
-  })
-}
+ReactDOM.render(
+  <MessageList />,
+  document.getElementById('root'),
+);
