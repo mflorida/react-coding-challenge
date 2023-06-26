ea1f7c1d148d00892af3e6ca2421b3aa583f4f02
Mark M. Florida <4173034+mflorida@users.noreply.github.com>
2023-06-25 20:58:21 -0500

    Rename message-list.js to message-list-class.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import MessageList from './components/message-list';
+import MessageList from './components/message-list-class';
 
 ReactDOM.render(
   <MessageList />,
