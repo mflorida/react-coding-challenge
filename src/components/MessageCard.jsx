8feb294b13233ccfd1b4eedefa3ce74e892d7451
Mark M. Florida <4173034+mflorida@users.noreply.github.com>
2023-06-26 01:50:24 -0500

    Style tweaks; using <button> element for 'clear'

--- src/components/MessageCard.jsx
+++ src/components/MessageCard.jsx
@@ -3,11 +3,10 @@ import './MessageCard.css';
 
 export function MessageCard({ message: msg }) {
   return (
-    <div className={`message-card ${msg.type}-message flex items-top justify-between`}>
+    <div className={`message-card ${msg.type}-message flex items-top justify-between rounded-sm shadow`}>
       <h4 className={'message-text w-full m-0'}>{msg.message}</h4>
       <h5 className={'clear-message w-full m-0 text-right'}>
-        <>&times; </>
-        clear
+        <button type={'button'} className={'rounded-xs'}>clear</button>
       </h5>
     </div>
   );
