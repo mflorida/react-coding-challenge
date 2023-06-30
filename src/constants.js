5a9722f45a1cac70fffaa96a81b4100e275cb634
Mark M. Florida <4173034+mflorida@users.noreply.github.com>
2023-06-30 11:47:13 -0500

    Add messageStoreMap to constants

--- src/constants.js
+++ src/constants.js
@@ -1,43 +1,19 @@
-export const messageTypes = {
-  0: {
-    type: 'none',
-    priority: 0,
-    label: 'None',
-    color: '#505050',
-  },
-  1: {
-    type: 'error',
-    priority: 1,
-    label: 'Error',
-    color: '#f56236',
-  },
-  2: {
-    type: 'warning',
-    priority: 2,
-    label: 'Warning',
-    color: '#fce788',
-  },
-  3: {
-    type: 'info',
-    priority: 3,
-    label: 'Info',
-    color: '#88fca3',
-  },
-};
+export const devmode = () => /devmode/i.test(window.location.hash);
 
 // Map key matches priority
 export const messageTypeMap = new Map([
-  [0, {
-    type: 'none',
-    priority: 0,
-    label: 'None',
-    color: '#505050',
-  }],
+  // [0, {
+  //   type: 'none',
+  //   priority: 0,
+  //   label: 'None',
+  //   color: '#505050',
+  // }],
   [1, {
     type: 'error',
     priority: 1,
     label: 'Error',
-    color: '#f56236',
+    // color: '#f56236',
+    color: '#ff6969',
   }],
   [2, {
     type: 'warning',
@@ -53,7 +29,56 @@ export const messageTypeMap = new Map([
   }],
 ]);
 
-// Indexes line up with priority values
-export const messageTypeList = [...messageTypeMap.values()].map(o => o.type);
+// Create object using 'type' as the keys
+export const messageTypes = Array.from(messageTypeMap).reduce((types, [i, def]) => {
+  return {
+    ...types,
+    [def.type]: def
+  }
+}, {});
 
-console.log('messageTypeList', messageTypeList);
+export function getMessageType(priority) {
+  return messageTypeMap.get(priority).type;
+}
+
+export function getMessageTypeLabel(priority) {
+  return messageTypeMap.get(priority).label;
+}
+
+export const nope = (...args) => [...args];
+
+export const messageStoreMap = new Map([
+  [1, {
+    priority: 1,
+    type: 'error',
+    label: 'Error',
+    // color: '#f56236',
+    color: '#ffa098',
+    messages: [],
+    setMessages: nope,
+  }],
+  [2, {
+    priority: 2,
+    type: 'warning',
+    label: 'Warning',
+    color: '#fce788',
+    messages: [],
+    setMessages: nope,
+  }],
+  [3, {
+    priority: 3,
+    type: 'info',
+    label: 'Info',
+    color: '#88fca3',
+    messages: [],
+    setMessages: nope,
+  }],
+  [4, {
+    priority: 4,
+    type: 'message',
+    label: 'Message',
+    color: '#d0d0d0',
+    messages: [],
+    setMessages: nope,
+  }],
+]);
