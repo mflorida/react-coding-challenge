5b7bd413649ed8d97231aa2b54c59eb0bfc88855
Mark M. Florida <4173034+mflorida@users.noreply.github.com>
2023-06-30 11:55:18 -0500

    Refactor MessageCard
    Updated structure of MessageCard HTML, updated styling, added fade-in animation for new messages

--- src/components/MessageCard/index.jsx
+++ src/components/MessageCard/index.jsx
@@ -1,20 +1,27 @@
 import React from 'react';
 import './style.css';
 
-export function MessageCard({ message, clear, id }) {
+export function MessageCard({ message, clear }) {
+  const classes = [
+    'message-card',
+    `${message.type}-message`,
+    'flex',
+    'items-top',
+    'justify-between',
+    'rounded-sm',
+    'shadow',
+  ];
+
   return (
-    <div className={`message-card flex items-top justify-between rounded-sm shadow`}>
+    <div data-id={message.id} className={classes.join(' ').trim()}>
       <h4 className={'message-text w-full m-0'}>{message.message}</h4>
-      <h5 className={'clear-message w-full m-0 text-right'}>
-        <button
-          type={'button'}
-          className={'rounded-xs'}
-          data-id={id}
-          onClick={clear}
-        >
-          clear
-        </button>
-      </h5>
+      <button
+        type={'button'}
+        className={'flex items-center justify-center rounded-xs'}
+        onClick={clear}
+      >
+        <span>&times;</span>
+      </button>
     </div>
   );
 }
